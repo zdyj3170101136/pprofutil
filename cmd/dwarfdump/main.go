5da944fe54913af6721ae4619fd6aeecfb057c44
杨杰 <zdyj3170101136@gmail.com>
2022-12-08 11:22:18 +0800

    cmd/dwarfdump:init

---
+++ cmd/dwarfdump/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"debug/dwarf"
+	"debug/elf"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := elf.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	d, err := f.DWARF()
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := d.Reader()
+	var lastE = &dwarf.Entry{}
+	for e, err := r.Next(); err == nil; e, err = r.Next() {
+		if e != nil {
+			log.Println(e)
+		}
+		// there is no more section
+		if lastE == nil && e == nil {
+			break
+		}
+		lastE = e
+	}
+}
